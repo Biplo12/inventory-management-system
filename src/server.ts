74ef4c5fbd5d72b917b396786ee7c0b4e376bee5
Robert Siński <robert.sinski@outlook.com>
2025-01-09 15:45:12 +0100

    chore: add body-parser dependency and improve server port handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`[server]: Server is running on port ${process.env.PORT}`);
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running on port ${PORT}`);
 });
